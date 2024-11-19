c8f9a1c317b1c9afd40204717fd9e6d25bbcbf4d
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2024-11-19 07:51:05 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,8 @@
-
+const getCSS = (variavel) => {
+  return getComtedStyle(document.body).getPropertyValue(variavel)
+};
+const tickConfig = {
+  color:getCSS('--primary-color'),
+  size:15,
+  family: getCSS('--font')
+    
